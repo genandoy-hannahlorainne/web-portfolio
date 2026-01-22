c0c68f13a909302933fe115e0483d238a1492df0
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2026-01-22 17:20:13 +0800

    feat: add footer with contact section and social links

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from './components/navbar/navbar';
 import { AboutComponent } from './components/about/about';
 import { TechStackComponent } from './components/tech-stack/tech-stack';
 import { ProjectsComponent } from './components/projects/projects';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, AboutComponent, TechStackComponent, ProjectsComponent],
+  imports: [NavbarComponent, AboutComponent, TechStackComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
