98da16c10563ab05c1694310c47c9714681fedcd
Hannah Lorainne Genandoy <lorainneh540@gmail.com>
2026-01-26 00:27:32 +0800

    fix: typing animation across all screen sizes with responsive font scaling

--- portfolio/src/app/components/navbar/navbar.ts
+++ portfolio/src/app/components/navbar/navbar.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
