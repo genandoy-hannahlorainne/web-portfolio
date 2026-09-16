44ccb869191130fe88fa12b9710aa2137b55cdc2
genandoy-hannahlorainne <genandoyhl@gmail.com>
2026-09-16 23:54:42 +0800

    feat: update about section with dynamic image positioning and new profile image

--- portfolio/src/app/components/about/about.ts
+++ portfolio/src/app/components/about/about.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './about.scss',
 })
 export class AboutComponent {
+  imageX = 0;
+  imageY = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+  onPointerMove(event: MouseEvent): void {
+    const x = event.clientX / window.innerWidth - 0.5;
+    const y = event.clientY / window.innerHeight - 0.5;
+
+    this.imageX = x * 12;
+    this.imageY = y * 12;
+  }
+
+  @HostListener('document:mouseleave')
+  onPointerLeave(): void {
+    this.imageX = 0;
+    this.imageY = 0;
+  }
 
 }
